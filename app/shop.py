2315bd6a1f86a73dd3b34a49851b1d64c6613f82
Ze Mendes <mendes.j@ua.pt>
2023-10-15 19:17:19 +0100

    Search for products in shop page

--- app/shop.py
+++ app/shop.py
@@ -6,11 +6,21 @@ from . import db
 shops = Blueprint("shops", __name__)
 
 
-@shops.route("/shop", methods=["GET"])
+@shops.route("/shop", methods=["GET", "POST"])
 def shop():
-    query = text("SELECT * FROM product")
-    products = db.session.execute(query).fetchall()
+    if request.method == "GET":
+        query = text("SELECT * FROM product")
+        products = db.session.execute(query).fetchall()
 
-    print(products)
+        print(products)
 
-    return render_template("shop.html", products=products)
+        return render_template("shop.html", products=products)
+    
+    else: #POST
+        print("POST")
+        if 'search' in request.form:
+            search_value = request.form['search_value']
+            query = text("SELECT * FROM product WHERE name LIKE :search")
+            products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+            return render_template("shop.html", products=products, default_value=search_value.strip())
