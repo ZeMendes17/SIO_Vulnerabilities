8e9fd9636381e0f5abae633f3b0ccd29507ba246
Ze Mendes <mendes.j@ua.pt>
2023-10-15 21:47:53 +0100

    Sorting is know working on products page

--- app/shop.py
+++ app/shop.py
@@ -8,6 +8,7 @@ shops = Blueprint("shops", __name__)
 
 @shops.route("/shop", methods=["GET", "POST"])
 def shop():
+    print("shop")
     if request.method == "GET":
         query = text("SELECT * FROM product")
         products = db.session.execute(query).fetchall()
@@ -17,10 +18,43 @@ def shop():
         return render_template("shop.html", products=products)
     
     else: #POST
-        print("POST")
         if 'search' in request.form:
             search_value = request.form['search_value']
             query = text("SELECT * FROM product WHERE name LIKE :search")
             products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
 
             return render_template("shop.html", products=products, default_value=search_value.strip())
+        
+        elif 'option' in request.form:
+            op = int(request.form['option'])
+            options = ["nothing", "rating", "priceDesc", "priceAsc"]
+            selected = options[op]
+
+            return selected
+        
+    
+@shops.route("/shop/<option>", methods=["GET"])        
+def sort(option):
+    if option == "nothing":
+        return redirect(url_for("shops.shop"))
+
+    elif option == "rating":
+        # query = text("SELECT * FROM product ORDER BY rating DESC")
+        # products = db.session.execute(query).fetchall()
+        
+        # return render_template("shop.html", products=products, option=option)
+        pass
+    
+    elif option == "priceDesc":
+        query = text("SELECT * FROM product ORDER BY price DESC")
+        products = db.session.execute(query).fetchall()
+        
+        return render_template("shop.html", products=products, option=option)
+
+    
+    elif option == "priceAsc":
+        query = text("SELECT * FROM product ORDER BY price ASC")
+        products = db.session.execute(query).fetchall()
+        
+        return render_template("shop.html", products=products, option=option)
+    
