d4a90976eee96e50f5fcc4bd0f2fadc0e4764c90
DuarteCruz31 <duarteccruz8@gmail.com>
2023-10-16 15:12:27 +0100

    trying to merge ta dificil

--- app/shop.py
+++ app/shop.py
@@ -30,6 +30,7 @@ def shop():
         return render_template("shop.html", products=products, number_of_items=number_of_items)
     
     else: #POST
+        print("POST in shop")
         if 'search' in request.form:
             search_value = request.form['search_value']
             query = text("SELECT * FROM product WHERE name LIKE :search")
@@ -39,35 +40,106 @@ def shop():
         
         elif 'option' in request.form:
             op = int(request.form['option'])
-            options = ["nothing", "rating", "priceDesc", "priceAsc"]
+            options = ["nothing", "rating", "priceDesc", "priceAsc", "clothing", "accessories", "miscellaneous"]
             selected = options[op]
 
             return selected
         
     
-@shops.route("/shop/<option>", methods=["GET"])        
+@shops.route("/shop/<option>", methods=["GET", "POST"])        
 def sort(option):
-    if option == "nothing":
-        return redirect(url_for("shops.shop"))
+    if request.method == "GET":
+        if option == "nothing":
+            return redirect(url_for("shops.shop"))
 
-    elif option == "rating":
-        query = text("SELECT * FROM product ORDER BY rating DESC")
-        products = db.session.execute(query).fetchall()
+        elif option == "rating":
+            query = text("SELECT * FROM product ORDER BY rating DESC")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
         
-        return render_template("shop.html", products=products, option=option)
-    
-    elif option == "priceDesc":
-        query = text("SELECT * FROM product ORDER BY price DESC")
-        products = db.session.execute(query).fetchall()
-        
-        return render_template("shop.html", products=products, option=option)
+        elif option == "priceDesc":
+            query = text("SELECT * FROM product ORDER BY price DESC")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
 
-    
-    elif option == "priceAsc":
-        query = text("SELECT * FROM product ORDER BY price ASC")
-        products = db.session.execute(query).fetchall()
         
-        return render_template("shop.html", products=products, option=option)
+        elif option == "priceAsc":
+            query = text("SELECT * FROM product ORDER BY price ASC")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
+        
+        elif option == "clothing":
+            query = text("SELECT * FROM product WHERE categorie = 'clothing'")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
+        
+        elif option == "accessories":
+            query = text("SELECT * FROM product WHERE categorie = 'accessories'")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
+        
+        elif option == "miscellaneous":
+            query = text("SELECT * FROM product WHERE categorie = 'miscellaneous'")
+            products = db.session.execute(query).fetchall()
+            
+            return render_template("shop.html", products=products, option=option)
+        
+        else:
+            return redirect(url_for("shops.shop"))
+        
+    elif request.method == "POST":
+        if 'search' in request.form:
+            search_value = request.form['search_value']
+            
+            if option == "rating":
+                query = text("SELECT * FROM product WHERE name LIKE :search ORDER BY rating DESC")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            elif option == "priceDesc":
+                query = text("SELECT * FROM product WHERE name LIKE :search ORDER BY price DESC")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            elif option == "priceAsc":
+                query = text("SELECT * FROM product WHERE name LIKE :search ORDER BY price ASC")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            elif option == "clothing":
+                query = text("SELECT * FROM product WHERE name LIKE :search AND categorie = 'clothing'")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            elif option == "accessories":
+                query = text("SELECT * FROM product WHERE name LIKE :search AND categorie = 'accessories'")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            elif option == "miscellaneous":
+                query = text("SELECT * FROM product WHERE name LIKE :search AND categorie = 'miscellaneous'")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+            else:
+                query = text("SELECT * FROM product WHERE name LIKE :search")
+                products = db.session.execute(query, {"search": "%" + search_value + "%"}).fetchall()
+
+                return render_template("shop.html", products=products, option=option, default_value=search_value.strip())
+            
+        else:
+            return redirect(url_for("shops.shop"))
     
 @shops.route("/shop/add_to_cart/<int:id>", methods=["POST"])
 def add_to_cart(id):
