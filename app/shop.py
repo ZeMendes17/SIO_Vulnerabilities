72046ece87aad49b82e61293ea51028e48691a9a
DuarteCruz31 <duarteccruz8@gmail.com>
2023-10-16 15:22:22 +0100

    changed shop

--- app/shop.py
+++ app/shop.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from .models import User
 from sqlalchemy import text
 from . import db
+from flask_login import current_user
 
 shops = Blueprint("shops", __name__)
 
@@ -13,9 +14,20 @@ def shop():
         query = text("SELECT * FROM product")
         products = db.session.execute(query).fetchall()
 
-        print(products)
+        # get number of items in cart
+        query = text("SELECT * FROM cart WHERE customer_id =" + str(current_user.id))
 
-        return render_template("shop.html", products=products)
+        cart = db.session.execute(query).fetchone()
+
+        if cart is not None:
+            query = text(
+                "SELECT COUNT(*) FROM cart_product WHERE cart_id =" + str(cart.id)
+            )
+            number_of_items = db.session.execute(query).fetchone()[0]
+        else:
+            number_of_items = 0
+
+        return render_template("shop.html", products=products, number_of_items=number_of_items)
     
     else: #POST
         print("POST in shop")
@@ -129,3 +141,34 @@ def sort(option):
         else:
             return redirect(url_for("shops.shop"))
     
+@shops.route("/shop/add_to_cart/<int:id>", methods=["POST"])
+def add_to_cart(id):
+    query = text("SELECT * FROM product WHERE id =" + str(id))
+    product = db.session.execute(query).fetchone()
+
+    query = text("SELECT * FROM cart WHERE customer_id =" + str(current_user.id))
+    cart = db.session.execute(query).fetchone()
+
+    query = text(
+        "SELECT * FROM cart_product WHERE cart_id ="
+        + str(cart.id)
+        + " AND product_id ="
+        + str(product.id)
+    )
+    product_in_cart = db.session.execute(query).fetchone()
+
+    if product_in_cart is None:
+        query = text(
+            "INSERT INTO cart_product (cart_id, product_id) VALUES ("
+            + str(cart.id)
+            + ","
+            + str(product.id)
+            + ")"
+        )
+        db.session.execute(query)
+        db.session.commit()
+        flash("Product added to cart!", "success")
+    else:
+        flash("Product already in cart!", "error")
+
+    return redirect("/shop")
